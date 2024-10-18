9934b42dd4967a4ce4185612a6264f0165c055dd
Bruno Campos <brunosopmac@gmail.com>
2024-10-18 17:34:46 -0300

    Heading levels should only increase by one

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -11,7 +11,7 @@ const Profile = ({ name, avatar, profile, location }: Props) => {
       <img src={avatar} alt={name} className="rounded-full w-22 h-22" />
       <div className="flex flex-col gap-1 items-center">
         <h1 className="text-2xl font-bold">{name}</h1>
-        <h3 className="text-xxs font-bold text-lime-350">{location}</h3>
+        <h2 className="text-xxs font-bold text-lime-350">{location}</h2>
       </div>
       <p className="text-xxs leading-6">"{profile}"</p>
     </div>
